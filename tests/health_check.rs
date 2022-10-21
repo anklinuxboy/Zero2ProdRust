c90ca49d64cb8a55250bd8186d56c02de398c0ca
Ankit Sharma <kitsharmawork@gmail.com>
2022-10-20 20:53:41 -0400

    Added health check endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -28,3 +28,48 @@ fn spawn_app() -> String {
 
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=ankit%20sharma&email=email%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_missing() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=ankit%20sharma", "missing email"),
+        ("email=email%40gmail.com", "missing name"),
+        ("", "missing both email and gmail"),
+    ];
+
+    for (body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("failed to execute");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The api did failed with code 400 when payload was {}",
+            error_message
+        );
+    }
+}
