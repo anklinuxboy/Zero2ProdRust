b8fe01b14b946c914dce7f0d5ae77d8de9b0392f
Ankit Sharma <kitsharmawork@gmail.com>
2022-10-30 23:39:33 -0400

    Added telemetry

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{HttpResponse, web};
 use sqlx::PgPool;
 use chrono::Utc;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -13,21 +14,35 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     conn_pool: web::Data<PgPool>,
 ) -> HttpResponse {
+    let request_id = Uuid::new_v4();
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+
+    let _request_span_guard = request_span.enter();
+    let query_span = tracing::info_span!("Saving new subscriber details in DB");
+
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
-        Uuid::new_v4(),
+        request_id,
         form.email,
         form.name,
         Utc::now()
     )
         .execute(conn_pool.get_ref())
+        .instrument(query_span)
         .await {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            tracing::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
--- src/startup.rs
+++ src/startup.rs
@@ -2,6 +2,7 @@ use crate::routes::{health_check, subscribe};
 use std::net::TcpListener;
 use actix_web::{web, App, HttpServer};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use sqlx::PgPool;
 
 pub fn run(
@@ -11,6 +12,7 @@ pub fn run(
     let conn_pool = web::Data::new(conn_pool);
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(conn_pool.clone())
