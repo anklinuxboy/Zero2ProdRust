dcd3741e8706070b7509ef34606ef2ad7fa5ab1d
Ankit Sharma <kitsharmawork@gmail.com>
2022-10-31 13:39:33 -0400

    cargo fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,11 @@
 use once_cell::sync::Lazy;
+use secrecy::ExposeSecret;
 use sqlx::{Executor, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use secrecy::ExposeSecret;
 
 static TRACING: Lazy<()> = Lazy::new(|| {
     let default_filter_level = "info".to_string();
